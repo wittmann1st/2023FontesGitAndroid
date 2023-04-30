2ab47c12d2424d5e67ccc33ead39fd148de2cb55
wittmann1st <wittmann1st@gmail.com>
2023-04-30 12:05:07 -0300

    Uso dos métodos  Get e Set

--- AppListaCurso/app/src/main/java/devandroid/wittmann/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/wittmann/applistacurso/view/MainActivity.java
@@ -10,12 +10,49 @@ import devandroid.wittmann.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Objeto
         pessoa = new Pessoa();
+        // Atribuir dados ao Objeto
+        pessoa.setPrimeiroNome("Roberto");
+        pessoa.setSobreNome("Wittmann");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("19 9 9999 9999");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Carmela");
+        outraPessoa.setSobreNome("Wittmann");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("19 9 8888 9999");
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+
+
     }
 }
